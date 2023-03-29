8a08beed1a9881d05c472caeefbf8e073a828757
lenokvadim62 <129267002+lenokvadim62@users.noreply.github.com>
2023-03-29 11:29:59 +0200

    Add files via upload

---
+++ Array5App.swift
@@ -0,0 +1,17 @@
+//
+//  Array5App.swift
+//  Array5
+//
+//  Created by Oleg Savelyev on 26.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Array5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
